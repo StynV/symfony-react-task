0c496b249e5c42d4ada146abefe7fcf21d274ffe
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-12-04 17:02:32 +0100

    add very basic documentation

--- backend/src/Controller/MockDataController.php
+++ backend/src/Controller/MockDataController.php
@@ -11,16 +11,19 @@ class MockDataController extends AbstractController
     #[Route('/mock/data', name: 'app_mock_data')]
     public function index(): JsonResponse
     {
+        // Retrieve file
         $filePath = $this->getParameter(('kernel.project_dir')) . '/public/data/project.json';
 
         if (!file_exists($filePath)) {
+            // Throw error in case the file doesn't exist
             return new Jsonresponse(['error' => 'Mock data file not found']);
         }
 
+        // Read and decode file
         $jsonData = file_get_contents($filePath);
         $data = json_decode($jsonData, true);
 
-
+        // Return JSON
         return new JsonResponse($data);
     }
 }
