6419bfa87e803f0818f3a1b0219b18d86f091135
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-12-05 09:07:08 +0100

    lint app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-
   return (
     <main className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="bg-green-500">test</h1>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
