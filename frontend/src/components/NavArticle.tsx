0e611fe20b49662678907e32e71d974b43505f1d
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-12-05 14:29:19 +0100

    add accordion

--- frontend/src/components/NavArticle.tsx
+++ frontend/src/components/NavArticle.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { TreeEntity } from '../models/MockData'
 
 type props = {
@@ -5,13 +7,35 @@ type props = {
 }
 
 const NavArticle = ({ article }: props) => {
+  const [active, setActive] = useState(true)
+
   const navigate = () => {
     window.location.hash = `#${article.number}`
   }
 
   return (
     <>
-      <li className={`${parent && 'pl-4'} py-2 flex flex-row relative group`}>
+      <li
+        className={`${parent && 'pl-4'} py-2 flex flex-row relative group cursor-pointer`}
+        onClick={() => setActive((currentState) => !currentState)}
+      >
+        <svg
+          className={`w-5 h-5 mr-2 transform transition-transform ${
+            active ? 'rotate-90' : ''
+          }`}
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 5l7 7-7 7"
+          />
+        </svg>
+
         <p>
           {article.number} {article.title}
         </p>
@@ -24,11 +48,13 @@ const NavArticle = ({ article }: props) => {
         </button>
       </li>
 
-      <li className="pl-4">
-        {article.children?.map((childArticle) => (
-          <NavArticle key={article.id} article={childArticle} />
-        ))}
-      </li>
+      {active && (
+        <li className="pl-4 cursor-pointer">
+          {article.children?.map((childArticle) => (
+            <NavArticle key={article.id} article={childArticle} />
+          ))}
+        </li>
+      )}
     </>
   )
 }
