5f59a85399a9dc1ad4a88a298e92c53ca72cb981
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-12-05 08:45:03 +0100

    update documentation

--- backend/src/Controller/MockDataController.php
+++ backend/src/Controller/MockDataController.php
@@ -6,24 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * MockDataController
+ * 
+ * This controller provides an endpoint to fetches data from a file.
+ */
 class MockDataController extends AbstractController
 {
-    #[Route('/mock/data', name: 'app_mock_data')]
+    /**
+     * @Route("/mock-data", name="app_mock_data", methods={"GET"})
+     * 
+     * Parameters: None
+     * Responses: 
+     * - 200 OK: File exists, so we return the content as JSON.
+     * - 404 Not Found: File doesn't exist, so we return an error.
+     */
+    #[Route('/mock-data', name: 'app_mock_data')]
     public function index(): JsonResponse
     {
-        // Retrieve file
         $filePath = $this->getParameter(('kernel.project_dir')) . '/public/data/project.json';
 
         if (!file_exists($filePath)) {
-            // Throw error in case the file doesn't exist
             return new Jsonresponse(['error' => 'Mock data file not found']);
         }
 
-        // Read and decode file
         $jsonData = file_get_contents($filePath);
         $data = json_decode($jsonData, true);
 
-        // Return JSON
         return new JsonResponse($data);
     }
 }
